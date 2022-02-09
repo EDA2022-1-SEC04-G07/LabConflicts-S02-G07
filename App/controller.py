625871dc78eac55288890f8ae0d2b6aa76d0aa22
Cata1406 <m.ibanez@uniandes.edu.co>
2022-02-09 01:21:07 -0500

    cambios finales

--- App/controller.py
+++ App/controller.py
@@ -55,10 +55,5 @@ def loadTags(filename):
 
 def loadBooksTags(filename):
     # TODO: Modificación de Est-1 y Est-2 en el Lab 2
-<<<<<<< HEAD
     booktagsfile = cf.data_dir + filename
     return model.addBookTags(booktagsfile)
-=======
-    btfile = cf.data_dir + filename
-    return model.addBookTags(btfile)
->>>>>>> Est-2
